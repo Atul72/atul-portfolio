7450a7279ab1ff97134ce37ac0facd0bfc15761f
Atul72 <rai703537@gmail.com>
2024-12-26 01:02:15 +0530

    project section added

---
+++ app/_components/projects.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+const ProjectData = [
+  {
+    title: "Hello Project1",
+    techStack: ["React", "Next.js", "Tailwind CSS"],
+    image: "https://via.placeholder.com/150/hello",
+    link: "https://www.google.com",
+    github: "https://www.github.com",
+  },
+  {
+    title: "Hello Project2",
+    techStack: ["React", "Next.js", "Tailwind CSS", "TypeScript"],
+    image: "https://via.placeholder.com/150/hello",
+    link: "https://www.google.com",
+    github: "https://www.github.com",
+  },
+  {
+    title: "Hello Project3",
+
+    techStack: ["React", "Next.js", "Tailwind CSS"],
+    image: "https://via.placeholder.com/150/hello",
+    link: "https://www.google.com",
+    github: "https://www.github.com",
+  },
+];
+
+export default function Projects() {
+  return (
+    <div className="grid md:grid-cols-3 grid-cols-1 gap-4 mt-[10px]">
+      {ProjectData.map((project) => (
+        <div
+          key={project.title}
+          className=" p-4 items-center flex flex-col justify-center"
+        >
+          <Image
+            src={project.image}
+            alt="something"
+            width={200}
+            height={200}
+            className="rounded-md"
+          />
+          <div className="font-bold text-center text-localPrimary-300 text-2xl">
+            {project.title}
+          </div>
+          <div className="flex flex-wrap gap-2">
+            {project.techStack.map((tech) => (
+              <Badge key={tech}>{tech}</Badge>
+            ))}
+          </div>
+          <div className="flex flex-row gap-2 justify-evenly w-full mt-2 text-localPrimary-300 text-xl ">
+            <div className="hover:text-localPrimary-600 hover:underline hover:cursor-pointer">
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                View Project
+              </a>
+            </div>
+            <div className="hover:text-localPrimary-600 hover:underline hover:cursor-pointer">
+              <a
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Repo Link
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import { Header } from "@/app/_components/header";
 import Section from "./_components/section";
+import Projects from "./_components/projects";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <span className="line-after-components"></span>
-      <div className="mt-[100px]">
+      <div className="mt-[50px]">
         <Section />
+      </div>
+      <div className="flex justify-between items-center mt-[50px] text-[20px] md:text-[35px] font-bold text-localPrimary-500">
+        <div>Projects</div>
+        <div className="text-[16px] text-localPrimary-100 transition-all duration-200  hover:underline hover:text-localPrimary-500 hover:cursor-pointer">
+          See all projects
+        </div>
+      </div>
+      <span className="line-after-components"></span>
+      <div>
+        <Projects />
       </div>
     </div>
   );
