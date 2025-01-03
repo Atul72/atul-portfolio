74f270a999146dd36138d373fc32ade59d25b6ea
Atul72 <rai703537@gmail.com>
2025-01-03 15:54:38 +0530

    refactoring

--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Navigation = () => {
   return (
-    <ul className="flex gap-4 text-[15px] md:text-[18px] text-localText-400 font-semibold ">
+    <ul className="flex gap-4 text-[15px] md:text-[18px] text-localText-400  ">
       <li className="hover:underline hover:text-localText-600 transition-all duration-200">
         <Link href="/projects">Projects</Link>
       </li>
